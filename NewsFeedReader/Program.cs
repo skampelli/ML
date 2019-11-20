101cd1b0e7e5b68f3c82267098149e53f6615d03
skamp <skamp@DESKTOP-SSNEGCU>
2019-11-20 11:13:49 +0530

    Add project files.

---
+++ NewsFeedReader/Program.cs
@@ -0,0 +1,52 @@
+﻿using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
+using System;
+using System.IO;
+using System.Text;
+
+namespace NewsFeedReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetGoogleNewsFeed();
+        }
+
+        static void GetGoogleNewsFeed()
+        {
+            try
+            {
+                var newsapiclient = new NewsApiClient("c2e5293629af4a0d9e575e860a53c00e");
+                var articleResponse = newsapiclient.GetEverything(new EverythingRequest
+                {
+                    SortBy = SortBys.PublishedAt,
+                    PageSize = 100,
+                    Language = Languages.EN,
+                    Q = "wells fargo"
+                });
+
+                if (articleResponse.Status == Statuses.Ok)
+                {
+                    var sb = new StringBuilder();
+                    foreach (var article in articleResponse.Articles)
+                    {
+                        var line = $"{article.PublishedAt}\t{article.Title}";
+                        sb.AppendLine(line);
+                    }
+
+                    using (FileStream fs = File.Create("newsfeed.tsv"))
+                    {
+                        Byte[] info = new UTF8Encoding(true).GetBytes(sb.ToString());
+                        fs.Write(info, 0, info.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ PricePredicitonUsingNewsFeed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PricePredicitonUsingNewsFeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
